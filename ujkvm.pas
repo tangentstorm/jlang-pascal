6c7bd0d266f5671122f9d14eb7ef599122123247
tangentstorm <michal.wallace@gmail.com>
2023-04-19 22:16:57 -0400

    ujkvm: line-drawing chars, SymbolToIndex, fix fCharH

--- ujkvm.pas
+++ ujkvm.pas
@@ -34,7 +34,7 @@ type
   public
     constructor Create(TheOwner: TComponent); override;
     destructor Destroy; override;
-    procedure DrawChar(x,y:integer; c:char; fg,bg:TColor);
+    procedure DrawChar(x,y:integer; ch:WideChar; fg,bg:TColor);
     procedure Clear(const bg:TColor=$333333);
     procedure Rnd;
   published
@@ -85,7 +85,7 @@ end;
 constructor TJKVM.Create(TheOwner: TComponent);
 begin
   inherited Create(TheOwner);
-  fCharW:=15; fCharH:=28; fFontName := 'Fira Mono'; SetFontH(24);
+  fCharW:=15; fCharH:=29; fFontName := 'Fira Mono'; SetFontH(24);
   BoundsChanged; { to init fBmp }
 end;
 
@@ -95,8 +95,48 @@ begin
   inherited Destroy;
 end;
 
+// we support a subset of unicode characters. mostly box-drawing characters.
+// this maps the codepoint to the corresponding index in the bitmap font.
+function SymbolToIndex(codepoint:word):byte;
+begin
+ case codepoint of
+   32..127: result := codepoint;
+   // line drawing characters:
+   $250c: result := 16;
+   $252c: result := 17;
+   $2510: result := 18;
+   $251c: result := 19;
+   $253c: result := 20;
+   $2524: result := 21;
+   $2514: result := 22;
+   $2534: result := 23;
+   $2518: result := 24;
+   $2502: result := 25;
+   $2500: result := 26;
+   $2580: result := 127; // half-box character
+   $25A1: result := 255; // white box: the "missing character" character
+   otherwise result := byte(255);
+ end;
+end;
+
 procedure TJKVM.SetFontH(AValue: byte);
-  var i : byte;
+  var i : byte; cp: word;
+  // special symbols, including the 11 box drawing characters that j uses.
+  const symbols: array of word = (
+    $250c, $252c, $2510, $251c, $253c, $2524,
+    $2514, $2534, $2518, $2502, $2500, $25A1);
+
+  procedure MakeBitmapChar(ix:byte; s:WideString);
+  begin
+    with fFnt[SymbolToIndex(ord(s[1]))] do begin
+      FontName := fFontName;
+      FontHeight := fFontH;
+      FontQuality := fqSystemClearType;
+      FontAntialias:= true;
+      TextOut(0,0, UTF8Encode(s), BGRAWhite);
+    end;
+  end;
+
 begin
   if (fFontH=AValue) and (Assigned(fTmp)) then Exit;
   fFontH:=AValue;
@@ -105,13 +145,9 @@ begin
     for i := 0 to 255 do fFnt[i].Free;
   end;
   for i := 0 to 255 do fFnt[i] := TBGRABitmap.Create(fCharW,fCharH, BGRAPixelTransparent);
-  for i := 32 to 126 do with fFnt[i] do begin
-    FontName := fFontName;
-    FontHeight := fFontH;
-    FontQuality := fqSystemClearType;
-    FontAntialias:= true;
-    TextOut(1,0, chr(i), BGRAWhite);
-  end;
+  for i := 32 to 126 do MakeBitmapChar(i, chr(i));
+  for cp in symbols do MakeBitmapChar(SymbolToIndex(cp), WideChar(cp));
+  // 127 is cp $2580 : the half-box drawing character. i just drew it myself.
   fFnt[127].FillRect(0, 0, fCharW, fCharH div 2, BGRAWhite);
   fTmp := TBGRABitmap.Create(fCharW,fCharH, BGRAPixelTransparent);
 end;
@@ -137,12 +173,13 @@ begin fBmp.Draw(Canvas, 0, 0, true);
 end;
 
 
-procedure TJKVM.DrawChar(x, y: integer; c: char; fg, bg: TColor);
-  var r,w: PBGRAPixel; i:word;
+procedure TJKVM.DrawChar(x, y: integer; ch: WideChar; fg, bg: TColor);
+  var r,w: PBGRAPixel; i:word; ix:byte;
 begin
+  ix := SymbolToIndex(ord(ch));
   fBmp.FillRect(x*fCharW, y*fCharH, (x+1)*fCharW, (y+1)*fCharH, bg);
   // make a new colored char with alpha
-  r := fFnt[byte(c)].Data; w := fTmp.Data;
+  r := fFnt[ix].Data; w := fTmp.Data;
   for i := fTmp.NbPixels-1 downto 0 do begin
     w^ := fg;  w^.alpha := r^.alpha;
     inc(r); inc(w);
